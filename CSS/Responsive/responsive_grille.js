3198772484852de02148bc61cfa1647ac75f0617
Kinako <gaetan.muller2005@gmail.com>
2024-11-26 21:41:13 +0100

    delete script

--- CSS/Responsive/responsive_grille.js
+++
@@ -1,8 +0,0 @@
-// Sélectionne le bouton et les liens
-const toggleButton = document.querySelector('.menu-toggle');
-const menuLinks = document.querySelector('.menu-links');
-
-// Ajoute un gestionnaire d'événements au clic
-toggleButton.addEventListener('click', () => {
-  menuLinks.classList.toggle('active'); // Montre ou cache les liens
-});
